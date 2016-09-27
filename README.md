37db41a1dafdec936c4db011dfe93d26debab2ee
John Lapeyre <lapeyre.math122a@gmail.com>
2016-09-27 12:55:38 +0200

    Fill in package template files 	modified:   README.md 	modified:   src/EmpiricalCDFs.jl 	modified:   test/runtests.jl

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
 # EmpiricalCDFs
 
+Create and print empirical cummulative distribution functions (CDFs)
+
+```julia
+ cdf = EmpiricalCDF()
+ push!(cdf,x)
+ ...
+ print(ostr,cdf)
+```
+
+`push!(cdf,x)` adds a datum to the cdf.
+
+`appends!(cdf,a)` appends data to the cdf.
+
+`print(ostr,cdf)` prints the cdf.
+By default `2000` log spaced points of `cdf` are printed. Six fields are printed for each coordinate `x`:
+`log10(x)`, `log10(1-cdf_val)`, `log10(cdf_val)`, `x`, `cdf_val`, `1-cdf_val`.
+
+For general use, an iterator for points in the cdf would be useful. But, this is not implemented.
+
 [![Build Status](https://travis-ci.org/jlapeyre/EmpiricalCDFs.jl.svg?branch=master)](https://travis-ci.org/jlapeyre/EmpiricalCDFs.jl)
 
-[![Coverage Status](https://coveralls.io/repos/jlapeyre/EmpiricalCDFs.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/jlapeyre/EmpiricalCDFs.jl?branch=master)
+#[![Coverage Status](https://coveralls.io/repos/jlapeyre/EmpiricalCDFs.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/jlapeyre/EmpiricalCDFs.jl?branch=master)
 
-[![codecov.io](http://codecov.io/github/jlapeyre/EmpiricalCDFs.jl/coverage.svg?branch=master)](http://codecov.io/github/jlapeyre/EmpiricalCDFs.jl?branch=master)
+#[![codecov.io](http://codecov.io/github/jlapeyre/EmpiricalCDFs.jl/coverage.svg?branch=master)](http://codecov.io/github/jlapeyre/EmpiricalCDFs.jl?branch=master)
--- src/EmpiricalCDFs.jl
+++ src/EmpiricalCDFs.jl
@@ -1,5 +1,64 @@
 module EmpiricalCDFs
 
-# package code goes here
+export EmpiricalCDF
+
+doc"""
+EmpiricalCDF()
+
+Return an empirical CDF.
+"""
+
+immutable EmpiricalCDF{T <: Real}
+    xdata::Array{T,1}  # death times
+    nprint_pts::Int
+    logprint::Bool
+end
+
+EmpiricalCDF(nprint_pts::Int) = EmpiricalCDF(Array(Float64,0), nprint_pts, true)
+EmpiricalCDF() = EmpiricalCDF(2000,true)
+EmpiricalCDF(logprint::Bool) = EmpiricalCDF(2000,logprint)
+
+EmpiricalCDF(nprint_pts::Int, logprint::Bool) = EmpiricalCDF(Array(Float64,0), nprint_pts, logprint)
+
+Base.push!(cdf::EmpiricalCDF, time) = push!(cdf.xdata,time)
+
+Base.append!(cdf::EmpiricalCDF, times) = append!(cdf.xdata,times)
+
+Base.sort!(cdf::EmpiricalCDF) = sort!(cdf.data)
+
+function Base.print(cdf::EmpiricalCDF,fn::String)
+    ostr = open(fn,"w")
+    print(ostr,cdf)
+    close(ostr)
+end
+
+# Note that we sort in place
+function Base.print(ostr::IOStream, cdf::EmpiricalCDF)
+    x = cdf.xdata
+    sort!(x)
+    n = length(x)
+    xmin = x[1]
+    xmax = x[end]
+    local prpts
+    println(ostr, "# cdf of survival times")
+    if cdf.logprint
+        println(ostr, "# cdf: log spacing of coordinate")
+        prpts = logspace(log10(xmin),log10(xmax), cdf.nprint_pts)        
+    else
+        println(ostr, "# cdf: linear spacing of coordinate")
+        prpts = linspace(xmin,xmax, cdf.nprint_pts)            
+    end
+    println(ostr, "# cdf: number of points in cdf: $n")    
+    println(ostr, "# log10(t)  log10(P(t))  log10(1-P(t))  t  1-P(t)   P(t)")
+    j = 1
+    for i in 1:n-1  # don't print ordinate value of 1
+        xp = x[i]
+        if xp >= prpts[j]
+            j += 1
+            cdf_val = i/n
+            @printf(ostr,"%e\t%e\t%e\t%e\t%e\t%e\n", log10(xp),  log10(1-cdf_val), log10(cdf_val), xp, cdf_val, 1-cdf_val)
+        end
+    end
+end
 
 end # module
