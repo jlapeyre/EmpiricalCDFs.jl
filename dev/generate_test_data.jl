8aac58e6c8c9e16ff025c97dc21f578e89c1ce0c
John Lapeyre <lapeyre.math122a@gmail.com>
2016-10-02 23:51:20 +0200

    forgot to add these 	new file:   dev/generate_test_data.jl 	new file:   test/paretocdf.bin

---
+++ dev/generate_test_data.jl
@@ -0,0 +1,42 @@
+using EmpiricalCDFs
+
+immutable Pareto
+    alpha::Float64
+    x0::Float64
+end
+
+Pareto(alpha) = Pareto(alpha,1.0)
+Pareto() = Pareto(1.0)
+
+Base.rand(p::Pareto) = p.x0 * rand()^(-one(p.alpha)/p.alpha)
+
+function Base.rand(p::Pareto, n::Int)
+    a = Array(Float64, n)
+    for i in 1:n
+        a[i] = rand(p)
+    end
+    a
+end
+
+const odir = joinpath(Pkg.dir("EmpiricalCDFs"), "test")
+const ofile = joinpath(odir, "paretocdf.bin")
+
+function write_test_data()
+    d = Pareto()
+    cdf = EmpiricalCDF()
+    append!(cdf, rand(d,10^6))
+    sort!(cdf)
+    write(ofile, cdf)
+    nothing
+end
+
+import EmpiricalCDFs: mleKS, scanmle
+
+# julia> read_test_data()
+# EmpiricalCDFs.MLEKS{Float64}(2.0018144538762677,0.0010018144538762677,0.2488588894720618)
+
+function read_test_data()
+    cdf = readcdf(ofile)
+    mleks = mleKS(cdf)
+    scanmle(cdf,15)
+end
