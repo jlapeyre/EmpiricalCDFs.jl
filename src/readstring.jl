bcc8d111424cbc8d79db9947bd1467b75f72768c
John Lapeyre <lapeyre.math122a@gmail.com>
2016-10-03 17:53:04 +0200

    add 32 bit read function

--- src/readstring.jl
+++ src/readstring.jl
@@ -7,8 +7,14 @@ function readlengthandstring(io::IO)
     readstring(io,stringlength)
 end
 
-function readstring(io::IO,stringlength::Int)
+function readstring(io::IO,stringlength::Int32)
     buf = Array(UInt8,convert(Int64,stringlength))
+    readbytes!(io,buf)
+@compat  unsafe_string(pointer(buf))    
+end
+
+function readstring(io::IO,stringlength::Int64)
+    buf = Array(UInt8,stringlength)
     readbytes!(io,buf)
 @compat  unsafe_string(pointer(buf))    
 end
